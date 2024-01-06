27fd7bac6be35c513679f7a090beda537fdc9378
alex-moore-codes <alex.moore.codes@gmail.com>
2024-01-06 09:16:08 -0500

    Customize Radix UI theme

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,15 +1,25 @@
 'use client';
-import { Button, Container, TextArea, TextField } from '@radix-ui/themes';
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  TextArea,
+  TextField,
+} from '@radix-ui/themes';
 import React from 'react';
 
 export default function page() {
   return (
-    <Container size={'1'}>
+    <div className="max-w-2xl">
+      <Heading as="h1" mb={'3'}>
+        Submit a new issue
+      </Heading>
       <TextField.Root mb={'3'}>
         <TextField.Input placeholder="Give your issue a title..." />
       </TextField.Root>
       <TextArea placeholder="Describe your issue..." mb={'3'} />
       <Button>Submit Issue</Button>
-    </Container>
+    </div>
   );
 }
