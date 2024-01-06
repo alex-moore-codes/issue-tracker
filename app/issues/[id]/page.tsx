997715dc46ed319d6c34ef52bd1243a1f780fa1a
alex-moore-codes <alex.moore.codes@gmail.com>
2024-01-06 15:25:31 -0500

    added markdown preview

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -2,6 +2,7 @@ import prisma from '@/prisma/client';
 import { notFound } from 'next/navigation';
 import { Card, Container, Flex, Heading, Text } from '@radix-ui/themes';
 import IssueStatusBadge from '@/app/components/IssueStatusBadge';
+import ReactMarkdown from 'react-markdown';
 
 export default async function page({ params }: { params: { id: string } }) {
   const issue = await prisma.issue.findUnique({
@@ -15,11 +16,13 @@ export default async function page({ params }: { params: { id: string } }) {
   return (
     <Container>
       <Heading mb={'3'}>{issue.title}</Heading>
-      <Flex gap={'4'} mb={'3'}>
+      <Flex gap={'4'} mb={'5'}>
         <IssueStatusBadge status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
-      <Card>{issue.description}</Card>
+      <Card className="prose">
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
+      </Card>
     </Container>
   );
 }
