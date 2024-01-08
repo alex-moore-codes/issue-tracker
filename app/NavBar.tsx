411b2cb14389f4dc1f4f99c8cc6a094be9d9b349
alex-moore-codes <alex.moore.codes@gmail.com>
2024-01-08 17:00:43 -0500

    Show the current user

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,13 @@
 "use client";
 
-import { Box, Container, Flex } from "@radix-ui/themes";
+import {
+	Avatar,
+	Box,
+	Container,
+	DropdownMenu,
+	Flex,
+	Text,
+} from "@radix-ui/themes";
 import classNames from "classnames";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
@@ -51,7 +58,23 @@ export default function NavBar() {
 					</Flex>
 					<Box>
 						{status === "authenticated" && (
-							<Link href={"/api/auth/signout"}>Sign out</Link>
+							<DropdownMenu.Root>
+								<DropdownMenu.Trigger>
+									<Avatar
+										src={session.user!.image!}
+										fallback="?"
+										className="cursor-pointer"
+									/>
+								</DropdownMenu.Trigger>
+								<DropdownMenu.Content>
+									<DropdownMenu.Label>
+										<Text size="2">{session.user!.name}</Text>
+									</DropdownMenu.Label>
+									<DropdownMenu.Item>
+										<Link href={"/api/auth/signout"}>Sign out</Link>
+									</DropdownMenu.Item>
+								</DropdownMenu.Content>
+							</DropdownMenu.Root>
 						)}
 						{status !== "authenticated" && (
 							<Link href={"/api/auth/signin"}>Sign in</Link>
