07561505e3bcf033711a4e524e89b4ea49684eb6
alex-moore-codes <alex.moore.codes@gmail.com>
2024-01-09 17:19:33 -0500

    Refactor Assignee Select component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,38 +1,31 @@
 "use client";
+import Skeleton from "@/app/components/Skeleton";
 import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import Skeleton from "@/app/components/Skeleton";
 import toast, { Toaster } from "react-hot-toast";
 
 export default function AssigneeSelect({ issue }: { issue: Issue }) {
-   const {
-      data: users,
-      error,
-      isLoading,
-   } = useQuery<User[]>({
-      queryKey: ["users"],
-      queryFn: () => axios.get("/api/users").then((res) => res.data),
-      staleTime: 60_000,
-      retry: 3,
-   });
+   const { data: users, error, isLoading } = useUsers();
 
    if (error) return null;
 
    if (isLoading) return <Skeleton height={"2rem"} />;
 
+   async function assignIssue(userId: string) {
+      await axios
+         .patch(`/api/issues/${issue.id}`, {
+            assignedToUserId: userId || null,
+         })
+         .catch(() => toast.error("Changes could not be saved."));
+   }
+
    return (
       <>
          <Select.Root
             defaultValue={issue.assignedToUserId || ""}
-            onValueChange={async (userId) =>
-               await axios
-                  .patch(`/api/issues/${issue.id}`, {
-                     assignedToUserId: userId || null,
-                  })
-                  .catch(() => toast.error("Changes could not be saved."))
-            }
+            onValueChange={assignIssue}
          >
             <Select.Trigger />
             <Select.Content>
@@ -51,3 +44,11 @@ export default function AssigneeSelect({ issue }: { issue: Issue }) {
       </>
    );
 }
+
+const useUsers = () =>
+   useQuery<User[]>({
+      queryKey: ["users"],
+      queryFn: () => axios.get("/api/users").then((res) => res.data),
+      staleTime: 60_000,
+      retry: 3,
+   });
