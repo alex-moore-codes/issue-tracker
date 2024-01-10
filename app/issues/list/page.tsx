6b1b883795d48d8ec3ded9d305f468f2295c292f
alex-moore-codes <alex.moore.codes@gmail.com>
2024-01-10 09:38:03 -0500

    Sort issue

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -43,10 +43,17 @@ export default async function page({ searchParams }: Props) {
       ? searchParams.status
       : undefined;
 
+   const orderBy = columns
+      .map((column) => column.value)
+      .includes(searchParams.orderBy)
+      ? { [searchParams.orderBy]: "asc" }
+      : undefined;
+
    const issues = await prisma.issue.findMany({
       where: {
          status,
       },
+      orderBy,
    });
 
    return (
