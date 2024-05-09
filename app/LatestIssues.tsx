ea33f74a77325aad7da56ed40d552302ee4d455d
alex-moore-codes <alex.moore.codes@gmail.com>
2024-05-09 07:08:23 -0400

    Finalize for push

--- app/LatestIssues.tsx
+++ app/LatestIssues.tsx
@@ -4,47 +4,42 @@ import { IssueStatusBadge } from "./components";
 import Link from "@/app/components/Link";
 
 export default async function LatestIssues() {
-   const latestIssues = await prisma.issue.findMany({
-      orderBy: {
-         createdAt: "desc",
-      },
-      take: 5,
-      include: {
-         assignedToUser: true,
-      },
-   });
+  const latestIssues = await prisma.issue.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+    take: 5,
+    include: {
+      assignedToUser: true,
+    },
+  });
 
-   return (
-      <Card>
-         <Heading size={"3"} mb={"5"}>
-            Latest Issues
-         </Heading>
-         <Table.Root>
-            <Table.Body>
-               {latestIssues.map((issue) => (
-                  <Table.Row key={issue.id}>
-                     <Table.Cell>
-                        <Flex direction={"row"} justify={"between"}>
-                           <Flex direction={"column"} gap={"3"} align={"start"}>
-                              <Text size={"3"}>
-                                 <Link href={`/issues/${issue.id}`}>
-                                    {issue.title}
-                                 </Link>
-                              </Text>
-                              <IssueStatusBadge status={issue.status} />
-                           </Flex>
-                           {issue.assignedToUser && (
-                              <Avatar
-                                 src={issue.assignedToUser.image!}
-                                 fallback="?"
-                              />
-                           )}
-                        </Flex>
-                     </Table.Cell>
-                  </Table.Row>
-               ))}
-            </Table.Body>
-         </Table.Root>
-      </Card>
-   );
+  return (
+    <Card>
+      <Heading size={"3"} mb={"5"}>
+        Latest Issues
+      </Heading>
+      <Table.Root>
+        <Table.Body>
+          {latestIssues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                <Flex direction={"row"} justify={"between"}>
+                  <Flex direction={"column"} gap={"3"} align={"start"}>
+                    <Text size={"3"}>
+                      <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+                    </Text>
+                    <IssueStatusBadge status={issue.status} />
+                  </Flex>
+                  {issue.assignedToUser && (
+                    <Avatar src={issue.assignedToUser.image!} fallback="?" />
+                  )}
+                </Flex>
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </Card>
+  );
 }
--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -15,68 +15,68 @@ import { z } from "zod";
 type IssueData = z.infer<typeof issueSchema>;
 
 interface IssueFormProps {
-   heading: string;
-   issue?: Issue;
+  heading: string;
+  issue?: Issue;
 }
 
 export default function IssueForm({ heading, issue }: IssueFormProps) {
-   const {
-      register,
-      control,
-      handleSubmit,
-      formState: { errors },
-   } = useForm<IssueData>({ resolver: zodResolver(issueSchema) });
-   const router = useRouter();
-   const [error, setError] = useState("");
-   const [isSubmitting, setSubmitting] = useState(false);
-   const onSubmit = handleSubmit(async (data) => {
-      try {
-         setSubmitting(true);
-         if (issue) {
-            await axios.patch(`/api/issues/${issue.id}`, data);
-         } else {
-            await axios.post("/api/issues", data);
-         }
-         router.push("/issues/list");
-         router.refresh();
-      } catch (error) {
-         setSubmitting(false);
-         setError("An internal error occurred. Please try again.");
-         setTimeout(() => setError(""), 3000);
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueData>({ resolver: zodResolver(issueSchema) });
+  const router = useRouter();
+  const [error, setError] = useState("");
+  const [isSubmitting, setSubmitting] = useState(false);
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      if (issue) {
+        await axios.patch(`/api/issues/${issue.id}`, data);
+      } else {
+        await axios.post("/api/issues", data);
       }
-   });
+      router.push("/issues/list");
+      router.refresh();
+    } catch (error) {
+      setSubmitting(false);
+      setError("An internal error occurred. Please try again.");
+      setTimeout(() => setError(""), 3000);
+    }
+  });
 
-   return (
-      <>
-         <Heading mb={"3"}>{heading}</Heading>
-         <form onSubmit={onSubmit}>
-            <TextField.Root mb={"3"}>
-               <TextField.Input
-                  placeholder="Give your issue a title..."
-                  defaultValue={issue?.title}
-                  {...register("title")}
-               />
-            </TextField.Root>
-            <ErrorMessage>{errors.title?.message}</ErrorMessage>
-            <Controller
-               name="description"
-               defaultValue={issue?.description}
-               control={control}
-               render={({ field }) => (
-                  <SimpleMDE placeholder="Describe your issue..." {...field} />
-               )}
-            />
-            <ErrorMessage>{errors.description?.message}</ErrorMessage>
-            <Button disabled={isSubmitting}>
-               {issue ? "Update Issue" : "Submit Issue"}{" "}
-               {isSubmitting && <Spinner />}
-            </Button>
-         </form>
-         {error && (
-            <Callout.Root color="red" role="alert" className="mt-5">
-               <Callout.Text>{error}</Callout.Text>
-            </Callout.Root>
-         )}
-      </>
-   );
+  return (
+    <>
+      <Heading mb={"3"}>{heading}</Heading>
+      <form onSubmit={onSubmit}>
+        <TextField.Root mb={"3"}>
+          <TextField.Input
+            placeholder="Give your issue a title..."
+            defaultValue={issue?.title}
+            {...register("title")}
+          />
+        </TextField.Root>
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
+        <Controller
+          name="description"
+          defaultValue={issue?.description}
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Describe your issue..." {...field} />
+          )}
+        />
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
+        <Button disabled={isSubmitting}>
+          {issue ? "Update Issue" : "Submit Issue"}{" "}
+          {isSubmitting && <Spinner />}
+        </Button>
+      </form>
+      {error && (
+        <Callout.Root color="red" role="alert" className="mt-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+    </>
+  );
 }
