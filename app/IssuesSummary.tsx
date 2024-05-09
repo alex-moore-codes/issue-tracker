02f34bdf43adfc1f8a20bf0712dff9e7d072d7d8
alex-moore-codes <alex.moore.codes@gmail.com>
2024-05-09 07:14:10 -0400

    Fix build errors

--- app/IssuesSummary.tsx
+++ app/IssuesSummary.tsx
@@ -3,47 +3,47 @@ import { Card, Flex, Text } from "@radix-ui/themes";
 import Link from "next/link";
 
 interface Props {
-   open: number;
-   inProgress: number;
-   closed: number;
+  open: number;
+  inProgress: number;
+  closed: number;
 }
 
 export default function IssueSummary({ open, inProgress, closed }: Props) {
-   const containers: { label: string; value: number; status: Status }[] = [
-      {
-         label: "Open Issues",
-         value: open,
-         status: "OPEN",
-      },
-      {
-         label: "In Progress Issues",
-         value: inProgress,
-         status: "IN_PROGRESS",
-      },
-      {
-         label: "Closed Issues",
-         value: closed,
-         status: "CLOSED",
-      },
-   ];
+  const containers: { label: string; value: number; status: Status }[] = [
+    {
+      label: "Open Issues",
+      value: open,
+      status: "OPEN",
+    },
+    {
+      label: "In Progress Issues",
+      value: inProgress,
+      status: "IN_PROGRESS",
+    },
+    {
+      label: "Closed Issues",
+      value: closed,
+      status: "CLOSED",
+    },
+  ];
 
-   return (
-      <Flex direction={"row"} gap={"4"} align={"stretch"} justify={"between"}>
-         {containers.map((container) => (
-            <Card className="w-full">
-               <Flex direction={"column"} gap={"4"}>
-                  <Link
-                     href={`/issues/list?status=${container.status}`}
-                     className="tex-sm font-medium"
-                  >
-                     {container.label}
-                  </Link>
-                  <Text size={"7"} className="font-bold">
-                     {container.value}
-                  </Text>
-               </Flex>
-            </Card>
-         ))}
-      </Flex>
-   );
+  return (
+    <Flex direction={"row"} gap={"4"} align={"stretch"} justify={"between"}>
+      {containers.map((container, index) => (
+        <Card key={index} className="w-full">
+          <Flex direction={"column"} gap={"4"}>
+            <Link
+              href={`/issues/list?status=${container.status}`}
+              className="tex-sm font-medium"
+            >
+              {container.label}
+            </Link>
+            <Text size={"7"} className="font-bold">
+              {container.value}
+            </Text>
+          </Flex>
+        </Card>
+      ))}
+    </Flex>
+  );
 }
