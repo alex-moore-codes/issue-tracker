56a6facc46aa28eb4c44046c86fab3e0c223d349
alex-moore-codes <alex.moore.codes@gmail.com>
2024-05-09 07:12:38 -0400

    Fix build error

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,18 +1,18 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
-import AuthProvider from "./auth/authProvider";
+import AuthProvider from "./auth/AuthProvider";
 import { PropsWithChildren } from "react";
 import QueryClientProvider from "./QueryClientProvider";
 
 export function Providers({ children }: PropsWithChildren) {
-   return (
-      <QueryClientProvider>
-         <AuthProvider>
-            {/* <ThemeProvider attribute="class"> */}
-            {children}
-            {/* </ThemeProvider> */}
-         </AuthProvider>
-      </QueryClientProvider>
-   );
+  return (
+    <QueryClientProvider>
+      <AuthProvider>
+        {/* <ThemeProvider attribute="class"> */}
+        {children}
+        {/* </ThemeProvider> */}
+      </AuthProvider>
+    </QueryClientProvider>
+  );
 }
